8378330caa98fbcfb516c2cc8ec317c90aed53c9
aditiii1123ak <103371461+aditiii1123ak@users.noreply.github.com>
2024-11-14 18:50:29 +0530

    Add files via upload
    This Python script creates a simple voice-controlled assistant named "Jarvis" using the pyttsx3, speech_recognition, and Wikipedia libraries. The assistant can perform various tasks, such as searching Wikipedia, opening websites, playing music, and providing time updates, all through voice commands. This script showcases a voice assistant capable of performing multiple tasks with simple voice commands, making it easy to interact with your computer and access information or services without typing.

---
+++ jarvis.py
@@ -0,0 +1,87 @@
+import pyttsx3
+import speech_recognition as sr
+import datetime
+import wikipedia
+import webbrowser
+import os
+
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def wishMe():
+    hour = datetime.datetime.now().hour
+    if 0 <= hour < 12:
+        speak("Good Morning!")
+    elif 12 <= hour < 18:
+        speak("Good Afternoon!")
+    else:
+        speak("Good Evening!")
+    speak("I am Jarvis Sir. Please tell me how may I help you")
+
+
+def takeCommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.energy_threshold = 4000  # Adjusting microphone sensitivity
+        r.pause_threshold = 0.8  # Increase pause threshold
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Say that again please...")
+        return "None"
+    return query
+
+
+if __name__ == "__main__":
+    wishMe()
+    while True:
+        query = takeCommand().lower()
+
+        if 'wikipedia' in query:
+            speak('Searching Wikipedia...')
+            query = query.replace("wikipedia", "")
+            results = wikipedia.summary(query, sentences=2)
+            speak("According to Wikipedia")
+            speak(results)
+
+        elif 'open youtube' in query:
+            webbrowser.open("youtube.com")
+
+        elif 'open google' in query:
+            webbrowser.open("google.com")
+
+        elif 'open stackoverflow' in query:
+            webbrowser.open("stackoverflow.com")
+
+        elif 'open github' in query:
+            webbrowser.open("github.com")
+
+        elif 'open w3schools' in query:
+            webbrowser.open("w3schools.com")
+
+        elif 'play music' in query:
+            music_dir = 'C:\\Users\\Aditi\\Music'
+            songs = os.listdir(music_dir)
+            print(songs)
+            os.startfile(os.path.join(music_dir, songs[3]))
+
+        elif 'the time' in query:
+            strTime = datetime.datetime.now().strftime("%H:%M:%S")
+            speak(f"Sir, the time is {strTime}")
+
+        elif 'open code' in query:
+            codePath = "C:\\Users\\Haris\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"
+            os.startfile(codePath)
